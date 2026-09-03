7c81172ee71b6795a3e0dd14acf64be88268654a
Prepward <prepward@gmail.com>
2026-09-03 22:11:21 +0530

    Add assessment admin configuration

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,58 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# Import models
+from .models import (
+    Course,
+    Lesson,
+    Instructor,
+    Learner,
+    Question,
+    Choice,
+    Submission
+)
 
 
+# Choice inline
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+# Question inline
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+
+# Lesson inline
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
 
-# Register your models here.
+# Course admin
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
 
+# Question admin
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text']
+
+
+# Lesson admin
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
-
+# Register models
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Submission)
